09182629fe8bb0effdfb964e2850cea2da4fc772
IchHab4Euro <136733014+IchHab4Euro@users.noreply.github.com>
2023-12-26 14:34:58 +0100

    printField Methode hinzugefügt

--- Field.cpp
+++ Field.cpp
@@ -10,10 +10,34 @@ public:
         }
     }
     
+    virtual void placeship() = 0;
     void printField() const;
-
-    virtual void placeShip() = 0;
 
 private:
     char grid[10][10]; 
 };
+
+void Field::printField() const {
+    for (int i = -1; i < 11; i++)
+    {   
+        if (i == -1 || i == 0)
+        {
+            std::cout << " ";
+        } else {
+            std::cout << i << " ";
+        }
+    }
+    std::cout << std::endl;
+
+    char c = 'A';    
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << c << "|";
+        for (int j = 0; j < 10; j++)
+        {
+            std::cout << grid[i][j] << " ";
+        }
+        std::cout << "|" << std::endl;
+        c++;
+    }
+}
